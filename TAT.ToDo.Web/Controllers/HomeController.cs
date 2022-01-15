ce0d2ad7da7afa9fbc6dbf3edce6859fe8dc438c
Berat Tatlı <berat.tatli@prodyum.com>
2022-01-15 17:01:29 +0300

    15.01.2022 Update

--- TAT.ToDo.Web/Controllers/HomeController.cs
+++ TAT.ToDo.Web/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TAT.ToDo.Web.Models;
 
 namespace TAT.ToDo.Web.Controllers
 {
@@ -10,7 +11,14 @@ namespace TAT.ToDo.Web.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return View(new List<MüşteriViewModel>()
+            {
+                new MüşteriViewModel(){Ad="Berat"},
+                new MüşteriViewModel(){Ad="Berat2"},
+                new MüşteriViewModel(){Ad="Berat3"},
+                new MüşteriViewModel(){Ad="Berat4"},
+                new MüşteriViewModel(){Ad="Berat5"},
+            });
         }
     }
 }
---
+++ TAT.ToDo.Web/ViewComponents/Urunler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TAT.ToDo.Web.Models;
+
+namespace TAT.ToDo.Web.ViewComponents
+{
+    public class Urunler : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+            
+        }
+    }
+}
